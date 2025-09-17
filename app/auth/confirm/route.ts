d742b6192c7dc5478102ed1bd6ec9105928556ed
karan-patel61 <patel_karan_cool@hotmail.com>
2025-09-17 00:25:03 -0400

    changing sigup route

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,10 +1,10 @@
 import { type EmailOtpType } from '@supabase/supabase-js'
-import { NextResponse, type NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 
-export async function GET(request: NextRequest) {
+export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type') as EmailOtpType | null
